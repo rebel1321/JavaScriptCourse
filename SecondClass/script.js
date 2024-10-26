fc036328b39dbcdf41ff87f98cc66b89c6d62c67
Satyam <www.satyam200413@gmail.com>
2024-10-26 06:15:52 +0530

    Completed my second lecture on operators and conditional statement

--- SecondClass/script.js
+++ SecondClass/script.js
@@ -1,3 +1,24 @@
-// This is my second java class
-/*This is my second day on JavaScript Tutorial*/
-console.log("Welcome to JavaScript")
+// let number = prompt("Enter a number");
+// console.log(number);
+// if(number%5==0){
+//     console.log(number," is divisible by 5");
+// }else{
+//     console.log(number," is not divisible by 5");
+// }
+
+
+// Second question
+let score = prompt("Enter Score");
+if(80<= score && score<=100){
+    console.log("Grade is A");
+}else if(70<= score && score<=79){
+    console.log("Grade is B");
+}
+else if(60<= score && score<=69){
+    console.log("Grade is C");
+}
+else if(50<= score && score<=59){
+    console.log("Grade is D");
+}else if(0<= score && score<=49){
+    console.log("Grade is F");
+}
